64b7f795dda26e2b794767b5a329505536d0b947
Montse <79296842+lapanquecita@users.noreply.github.com>
2024-10-01 15:15:46 -0600

    Update cdmx.py

--- cdmx.py
+++ cdmx.py
@@ -13,6 +13,22 @@ import pandas as pd
 import plotly.graph_objects as go
 
 
+MESES = {
+    1: "Enero",
+    2: "Febrero",
+    3: "Marzo",
+    4: "Abril",
+    5: "Mayo",
+    6: "Junio",
+    7: "Julio",
+    8: "Agosto",
+    9: "Septiembre",
+    10: "Octubre",
+    11: "Noviembre",
+    12: "Diciembre",
+}
+
+
 def main():
     """
     Crea un mapa choropleth con los sismos registrados dentro de la CDMX.
@@ -129,12 +145,11 @@ def main():
         framewidth=2,
         showlakes=False,
         coastlinewidth=0,
-        landcolor="#1A1A2E",
+        landcolor="#092635",
     )
 
     fig.update_layout(
         showlegend=True,
-        legend_bgcolor="#16213E",
         legend_title=" <b>Magnitud del sismo</b>",
         legend_title_side="top center",
         legend_itemsizing="constant",
@@ -144,13 +159,13 @@ def main():
         legend_yanchor="bottom",
         legend_bordercolor="#FFFFFF",
         legend_borderwidth=1.0,
-        font_family="Quicksand",
+        font_family="Lato",
         font_color="#FFFFFF",
         font_size=18,
         margin={"r": 0, "t": 60, "l": 0, "b": 60},
         width=1280,
         height=1280,
-        paper_bgcolor="#16213E",
+        paper_bgcolor="#1B4242",
         annotations=[
             dict(
                 x=0.94,
@@ -177,7 +192,7 @@ def main():
                 y=-0.039,
                 xanchor="left",
                 yanchor="top",
-                text="Fuente: SSN (02/09/2024)",
+                text="Fuente: SSN (01/10/2024)",
                 font_size=22,
             ),
             dict(
@@ -202,5 +217,191 @@ def main():
     fig.write_image("./cdmx.png")
 
 
+def registros_anuales(año):
+    """
+    Crea un mapa choropleth con los sismos registrados dentro de la CDMX.
+    """
+
+    # Cargamos el CSV de terremotos.
+    df = pd.read_csv("./data.csv", parse_dates=["Fecha"], index_col="Fecha")
+
+    # Seleccionamos registros del año especificado.
+    df = df[df.index.year == año]
+
+    # Extraemos el nombre del estado.
+    df["estado"] = df["Referencia de localizacion"].apply(
+        lambda x: x.split(",")[-1].strip()
+    )
+
+    # Escogemos solamente sismos ocurridos en la CDMX.
+    df = df[df["estado"] == "CDMX"]
+
+    # Filtramos sismos sin magnitud.
+    df = df[df["Magnitud"] != "no calculable"]
+
+    # Convertimos la magnitud a float.
+    df["Magnitud"] = df["Magnitud"].astype(float)
+
+    # Iniciamos el string para nuestra anotación por mes.
+    por_mes = ["<b>Registros por mes</b>"]
+
+    # Creamos un DataFrame con los registros por mes.
+    df_mes = df.resample("ME").count()["Magnitud"]
+
+    # Iteramos sobre este nuevo DataFrame para crear nuestros totales por año.
+    for index, row in df_mes.items():
+        por_mes.append(f"{MESES[index.month]}: <b>{row}</b>")
+
+    # Contamos todos los sismos de nuestro DataFrame filtrado.
+    subtitulo = f"<b>{len(df)}</b> registros totales"
+
+    # Estas listas serán usadas para nuestro mapa Choropleth.
+    ubicaciones = list()
+    valores = list()
+
+    # Cargamos el GeoJSON de la CDMX.
+    geojson = json.load(open("./assets/Ciudad de México.json", "r", encoding="utf-8"))
+
+    # Iteramos sobre las alcaldías dentro del GeoJSON.
+    for item in geojson["features"]:
+        geo = item["properties"]["CVEGEO"]
+
+        # A cada alcaldía le asignamos el valor 1.
+        ubicaciones.append(geo)
+        valores.append(1)
+
+    fig = go.Figure()
+
+    # Primero creamos un mapa Choropleth donde solo se mostrarán los contornos de las alcaldías.
+    fig.add_traces(
+        go.Choropleth(
+            geojson=geojson,
+            locations=ubicaciones,
+            z=valores,
+            showscale=False,
+            featureidkey="properties.CVEGEO",
+            colorscale=["#000000", "#000000"],
+            marker_line_color="#FFFFFF",
+            marker_line_width=1.5,
+            zmin=0.0,
+            zmax=1.0,
+        )
+    )
+
+    # Esta lista de listas nos ayudará a definir el color de cada grupo de sismos.
+    # Así como su nombre y rango.
+    bins = [
+        [0, 0.99999, "#ea80fc", "< 1.0"],
+        [1.0, 1.9999, "#00e5ff", "De 1.0 a 1.9"],
+        [2.0, 2.9999, "#fdd835", "De 2.0 a 2.9"],
+        [3.0, 10, "#FFA500", "≥ 3.0"],
+    ]
+
+    # Iteramos sobre la lista anterior y creamos un Scattergeo para cada una.
+    for start, end, color, nombre in bins:
+        # Creamos un DataFrame temporal con el rango de sismos.
+        temp_df = df[df["Magnitud"].between(start, end)]
+
+        # Contamos el número de sismos.
+        cantidad = len(temp_df)
+
+        fig.add_traces(
+            go.Scattergeo(
+                lon=temp_df["Longitud"],
+                lat=temp_df["Latitud"],
+                marker_color=color,
+                marker_size=temp_df["Magnitud"] * 4,
+                marker_line_width=2.25,
+                marker_opacity=1.0,
+                marker_symbol="circle-open",
+                name=f"{nombre} ({cantidad} sismos)"
+                if cantidad != 1
+                else f"{nombre} ({cantidad} sismo)",
+            )
+        )
+
+    fig.update_geos(
+        fitbounds="geojson",
+        projection_type="mercator",        
+        framecolor="#FFFFFF",
+        framewidth=2,
+        showlakes=False,
+        coastlinewidth=0,
+        landcolor="#092635",
+    )
+
+    fig.update_layout(
+        showlegend=True,
+        legend_title=" <b>Magnitud del sismo</b>",
+        legend_title_side="top center",
+        legend_itemsizing="constant",
+        legend_x=0.07,
+        legend_y=0.02,
+        legend_xanchor="left",
+        legend_yanchor="bottom",
+        legend_bordercolor="#FFFFFF",
+        legend_borderwidth=1.0,
+        font_family="Lato",
+        font_color="#FFFFFF",
+        font_size=18,
+        margin={"r": 0, "t": 60, "l": 0, "b": 60},
+        width=1280,
+        height=1280,
+        paper_bgcolor="#1B4242",
+        annotations=[
+            dict(
+                x=0.94,
+                y=0.95,
+                xanchor="right",
+                yanchor="top",
+                text="<br>".join(por_mes),
+                borderpad=10,
+                bordercolor="#FFFFFF",
+                borderwidth=1.0,
+                bgcolor="#1B4242",
+                align="left",
+            ),
+            dict(
+                x=0.5,
+                y=1.015,
+                xanchor="center",
+                yanchor="top",
+                text=f"Sismos registrados con epicentro cerca o dentro de la <b>Ciudad de México</b> durante el {año}",
+                font_size=26,
+            ),
+            dict(
+                x=0.06,
+                y=-0.039,
+                xanchor="left",
+                yanchor="top",
+                text="Fuente: SSN (01/10/2024)",
+                font_size=22,
+            ),
+            dict(
+                x=0.5,
+                y=-0.039,
+                xanchor="center",
+                yanchor="top",
+                text=subtitulo,
+                font_size=22,
+            ),
+            dict(
+                x=0.96,
+                y=-0.039,
+                xanchor="right",
+                yanchor="top",
+                text="🧁 @lapanquecita",
+                font_size=22,
+            ),
+        ],
+    )
+
+    fig.write_image(f"./cdmx_{año}.png")
+
+
 if __name__ == "__main__":
     main()
+
+    # registros_anuales(2022)
+    # registros_anuales(2023)
+    # registros_anuales(2024)
